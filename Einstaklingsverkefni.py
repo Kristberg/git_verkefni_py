41e56360cd8e7b9f881ac598d2c3c742756d7509
Kristberg Rúnar Pálsson <kristbergpa209@nemi.tskoli.is>
2017-01-25 12:30:53 +0000

    Dæmi 1

---
+++ Einstaklingsverkefni.py
@@ -0,0 +1,10 @@
+#Kristberg Rúnar Pálsson
+#25.1.2017
+#Dæmi 1
+tala1 = int(input("Sláðu inn tölu "))
+tala2 = int(input("Sláðu inn aðra tölu "))
+summa = tala1 + tala2
+margf = tala1 * tala2
+print("Tölurnar lagðar saman eru:", summa)
+print("Tölurnar margfaldaðar saman eru:", margf)
+
